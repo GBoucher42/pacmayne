619b41a6af014653414b27a760ed991303027e94
GBoucher42 <gabboucher93@gmail.com>
2018-09-12 17:35:09 -0400

    Add pacman (#2)
    * Add pacman square and moves
    
    * move actions to more appropriate classes
    
    still temporary while waiting for game thread

--- PacMan/src/entities/PacMan.java
+++ PacMan/src/entities/PacMan.java
@@ -1,10 +1,37 @@
 package entities;
 
+import static configs.GameConfig.GAME_SIZE;
+import static configs.GameConfig.TILE_SIZE;
+
+import javafx.scene.shape.Rectangle;
+
 public class PacMan extends GameEntity{
+	private Rectangle shape;
 	
 	public void startMoving()
 	{
-		// TODO:
+		switch(velocity.getDirection()){
+		case UP:
+			if(shape.getY() >= TILE_SIZE * velocity.getSpeed())
+				shape.setY(shape.getY() - TILE_SIZE * velocity.getSpeed());
+			break;
+		case DOWN:
+			if(shape.getY() < GAME_SIZE - TILE_SIZE * velocity.getSpeed())
+				shape.setY(shape.getY() + TILE_SIZE * velocity.getSpeed());
+			break;
+		case LEFT:
+			if(shape.getX() >= TILE_SIZE * velocity.getSpeed())
+				shape.setX(shape.getX() - TILE_SIZE * velocity.getSpeed());
+			break;
+		case RIGHT:
+			if(shape.getX() < GAME_SIZE - TILE_SIZE * velocity.getSpeed())
+				shape.setX(shape.getX() + TILE_SIZE * velocity.getSpeed());
+			break;
+		default:
+			break;
+			
+		}
+
 	}
 	
 	public void stopMoving()
@@ -21,4 +48,14 @@ public class PacMan extends GameEntity{
 	{
 		// TODO:
 	}
+
+	public Rectangle getShape() {
+		return shape;
+	}
+
+	public void setShape(Rectangle shape) {
+		this.shape = shape;
+	}
+	
+	
 }
