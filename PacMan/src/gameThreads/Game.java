8b3a1f839a3e6256437e098d3646ef6919f88ad5
Unknown <gabboucher93@gmail.com>
2018-09-12 16:53:16 -0400

    move actions to more appropriate classes
    still temporary while waiting for game thread

--- PacMan/src/gameThreads/Game.java
+++ PacMan/src/gameThreads/Game.java
@@ -6,12 +6,15 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import static configs.GameConfig.GAME_SIZE;
 import static configs.GameConfig.TILE_SIZE;
+
+import entities.Direction;
+import entities.PacMan;
+import entities.Velocity;
 
 public class Game {
 	
-	Rectangle pacman;
+	PacMan pacman;
 	Stage stage;
 
 	public Game(Stage stage)
@@ -35,20 +38,26 @@ public class Game {
 		
 		switch(keyCode) {
 			case UP:
-				if(pacman.getY() >= TILE_SIZE)
-					pacman.setY(pacman.getY() - 25);
+				pacman.setDirection(Direction.UP);
+				pacman.startMoving();
 				break;
 			case DOWN:
-				if(pacman.getY() < GAME_SIZE - TILE_SIZE)
-					pacman.setY(pacman.getY() + 25);
+				pacman.setDirection(Direction.DOWN);
+				pacman.startMoving();
 				break;
 			case LEFT:
-				if(pacman.getX() >= TILE_SIZE)
-					pacman.setX(pacman.getX() - 25);
+				pacman.setDirection(Direction.LEFT);
+				pacman.startMoving();
 				break;
 			case RIGHT:
-				if(pacman.getX() < GAME_SIZE - TILE_SIZE)
-					pacman.setX(pacman.getX() + 25);
+				pacman.setDirection(Direction.RIGHT);
+				pacman.startMoving();
+				break;
+			case F:
+				pacman.setSpeed(2);
+				break;
+			case S:
+				pacman.setSpeed(1);
 				break;
 			default:
 				break;
@@ -56,9 +65,14 @@ public class Game {
 	}
 	
 	public void addPacMan() {
-		pacman = new Rectangle(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE);
-		pacman.setFill(Color.YELLOW);
+		pacman = new PacMan();
+		Rectangle pacmanView = new Rectangle(TILE_SIZE, TILE_SIZE, TILE_SIZE, TILE_SIZE);
+		pacmanView.setFill(Color.YELLOW);
+		pacman.setShape(pacmanView);
+		Velocity velocity = new Velocity();
+		velocity.setSpeed(1);
+		pacman.setVelocity(velocity);
 		Pane root = (Pane) stage.getScene().getRoot();
-		root.getChildren().add(pacman);
+		root.getChildren().add(pacman.getShape());
 	}
 }
