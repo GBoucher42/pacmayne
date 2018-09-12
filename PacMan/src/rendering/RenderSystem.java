087e378a2b5fe2b3a580ae0f58668f6c978b8de5
Unknown <gabboucher93@gmail.com>
2018-09-12 15:23:44 -0400

    Add pacman square and moves

--- PacMan/src/rendering/RenderSystem.java
+++ PacMan/src/rendering/RenderSystem.java
@@ -1,16 +1,35 @@
 package rendering;
 
-public class RenderSystem {
+import gameThreads.Game;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import static configs.GameConfig.GAME_SIZE;
 
-	// TODO: private canvas & viewport
+public class RenderSystem extends Application {
 	
 	public RenderSystem()
-	{
-		init();
-	}
+	{}
 	
-	public void init()
-	{
-		// TODO: init canvas with resolution
+	public Stage initStage(Stage primaryStage, int width, int height) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, width, height, Color.BLACK);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return primaryStage;
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage = initStage(primaryStage, GAME_SIZE, GAME_SIZE);
+		Game gameInstance = new Game(primaryStage);
+ 		gameInstance.run();
 	}
 }
