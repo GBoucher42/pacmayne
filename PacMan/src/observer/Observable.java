922a309fca81d792863a602cded000f6cee3c3c2
floriand59 <florian.deneuville@outlook.fr>
2018-09-13 16:16:25 -0400

    Observerdesignpattern (#6)
    * Observer design pattern
    
    * Changement erreur nommage

---
+++ PacMan/src/observer/Observable.java
@@ -0,0 +1,30 @@
+package observer;
+
+import java.util.ArrayList;
+
+public class Observable {
+
+	private ArrayList<IObserver> observerList = new ArrayList<IObserver>();
+	
+	public void register(IObserver observer) {
+		if(!observerList.contains(observer)) {
+			observerList.add(observer);
+		}
+	}
+	
+	public void unregister(IObserver observer) {
+		if(observerList.contains(observer)) {
+			observerList.remove(observer);
+		}
+	}
+	
+	public void notify(IObserver observer) {
+		observer.update();
+	}
+	
+	public void notifyAllObservers() {
+		for (IObserver observer: observerList) {
+			observer.update();
+		}
+	}
+}
