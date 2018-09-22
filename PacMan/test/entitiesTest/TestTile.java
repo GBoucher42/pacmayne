f168deb322166a240a8efd74017338e1cbcd3963
bilel <bilel.belhajd@iit.ens.tn>
2018-09-22 16:45:56 -0400

    New testTile

--- PacMan/test/entitiesTest/TestTile.java
+++ PacMan/test/entitiesTest/TestTile.java
@@ -1,18 +1,23 @@
 package entitiesTest;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import entities.Animatable;
 import entities.Collectable;
+import entities.Fruit;
 import entities.Gum;
 import entities.IGameEntity;
+import entities.Maze;
 import entities.SuperGum;
 import entities.Tile;
 import entities.TileType;
@@ -20,42 +25,56 @@ import entities.Wall;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class TestTile {
-	private Collectable item;
-	private IGameEntity entity;
+	private Collectable itemGum, itemSgum, itemFruit;
 	private TileType type;
 	private Tile tile;
-	private Gum gum;
-	private SuperGum sgum;
-	private Animatable animatable;
-          
- @BeforeAll
- void setup() {
-	tile=new Tile(0, 0, type.WALL);
-	item =new Gum(20, 20, 150, animatable);
-	item =new SuperGum(20, 200, 30, animatable);
-	tile.setGameEntity(item);
- }
- 
- @Test(expected =NullPointerException.class)
- public void ConsumeCollectableCoorectly(){
-	 tile.setCollectable(item);
-	 assertTrue(tile.hasCollectable());
-	 assertNotEquals(null,tile.consumeCollectable());
-	 
- }
- @Test(expected =NullPointerException.class)
- public void IswallCorrectly() {
-	 assertEquals(true, tile.isWall());}
- 
- @Test(expected =NullPointerException.class)
-public void DontisTileGum(){
-	 assertEquals(false, item.isTileGum());
-	 assertEquals(false, tile.isTileGum());
-	 }
- @Test(expected =NullPointerException.class)
-public void isTileSuperGum(){
-	 assertEquals(false, item.isTileSuperGum());
-	 assertEquals(false, tile.isTileSuperGum());
-	 }
- 
+
+	@BeforeAll
+	void setup() {
+		tile = new Tile(1, 1, type.CORRIDOR);
+		itemGum = new Gum(100, 1, 1, new Animatable("GUM"));
+		itemSgum = new SuperGum(200, 1, 100, new Animatable("SUPERGUM"));
+		itemFruit = new Fruit(500, 300, 200, new Animatable("SUPERGUM"));
+	}
+
+	@Test
+	public void ConsumeCollectable_Gum_Coorectly() {
+		tile.setGameEntity(itemGum);
+		tile.setCollectable(itemGum);
+		assertEquals(itemGum.getScoreValue(), tile.consumeCollectable());
+	}
+
+	@Test
+	public void ConsumeCollectable_SuperGum_Coorectly() {
+		tile.setGameEntity(itemSgum);
+		tile.setCollectable(itemSgum);
+		assertEquals(itemSgum.getScoreValue(), tile.consumeCollectable());
+	}
+
+	@Test
+	public void ConsumeCollectable_Fruit_Coorectly() {
+		tile.setGameEntity(itemFruit);
+		tile.setCollectable(itemFruit);
+		assertEquals(itemFruit.getScoreValue(), tile.consumeCollectable());
+	}
+
+	@Test
+	public void DontIsWall() {
+		assertNotEquals(tile.getType(), tile.isWall());
+	}
+
+	@Test
+	public void YesitsTileGum() {
+		tile.setGameEntity(itemGum);
+		tile.setCollectable(itemGum);
+		assertEquals(true, tile.isTileGum());
+	}
+
+	@Test
+	public void YesitsTileSuperGum() {
+		tile.setGameEntity(itemSgum);
+		tile.setCollectable(itemSgum);
+		assertEquals(true, tile.isTileSuperGum());
+	}
+
 }
